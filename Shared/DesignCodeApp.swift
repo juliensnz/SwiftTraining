d4144249c80f377e795f5aab7be6fb856a409285
Julien Sanchez <sumbobyboys@gmail.com>
2021-01-10 20:25:27 +0100

    Initial Commit

---
+++ Shared/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  Shared
+//
+//  Created by Julien Sanchez on 10/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
